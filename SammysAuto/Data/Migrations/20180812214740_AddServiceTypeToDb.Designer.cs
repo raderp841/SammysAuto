d240e015d218c06b7dab0f52ac757084a8a2275e
peter rader <raderp841@gmail.com>
2018-08-13 06:20:10 -0400

    partial button page work

--- SammysAuto/Data/Migrations/20180812214740_AddServiceTypeToDb.Designer.cs
+++ SammysAuto/Data/Migrations/20180812214740_AddServiceTypeToDb.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SammysAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180812214740_AddServiceTypeToDb")]
+    partial class AddServiceTypeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -184,8 +185,7 @@ namespace SammysAuto.Data.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
 
-                    b.Property<string>("Name")
-                        .IsRequired();
+                    b.Property<string>("Name");
 
                     b.HasKey("Id");
 
---
+++ SammysAuto/Models/IndividualButtonPartial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SammysAuto.Models
+{
+    public class IndividualButtonPartial
+    {
+        public string ButtonType { get; set; }
+        public string Action { get; set; }
+        public string Glyphicon { get; set; }
+        public string Text { get; set; }
+        public int? ServiceId { get; set; }
+
+        public string ActionParameters
+        {
+            get
+            {
+                var param = new StringBuilder(@"/");
+                if(ServiceId != 0 && ServiceId != null)
+                {
+                    param.Append(String.Format(""))
+                }
+            }
+        }
+    }
+}
